bdf83ddd3e9a9b4f3b8cec47102e0418df2d8b5b
ppinfer <73312445+ppinfer@users.noreply.github.com>
2021-05-07 09:47:02 +0200

    Add files via upload

---
+++ JavaScript/Actividad 1/MyJS.js
@@ -0,0 +1,10 @@
+
+
+
+  
+function cambiaParrafo() {
+  document.getElementById("demo").innerHTML = "Párrafo cambiado.";
+}
+function borraParrafo() {
+  document.getElementById("demo").style.display = 'none';
+}
